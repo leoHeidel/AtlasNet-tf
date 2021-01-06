35d82816f6b2e94562471f9b78cf06243618446a
Leo Heidelberger <leo.heidelberger@ferrari.polytechnique.fr>
2021-01-06 23:06:30 +0100

    Points cloud formula

--- dataset/rendering.py
+++ dataset/rendering.py
@@ -8,13 +8,28 @@ import dataset
 
 def render(obj_path, camera_mat, return_depth=False):
     fuze_trimesh = trimesh.load(obj_path)
-    scene = pyrender.Scene.from_trimesh_scene(fuze_trimesh)
+    if type(fuze_trimesh) == trimesh.base.Trimesh:
+        m = pyrender.Mesh.from_trimesh(fuze_trimesh)
+        scene = pyrender.Scene()
+        scene.add_node(pyrender.Node(mesh=m))
+    else:
+        assert type(fuze_trimesh) == trimesh.scene.scene.Scene, "Unrognized file"
+        scene = pyrender.Scene.from_trimesh_scene(fuze_trimesh)
     camera = pyrender.PerspectiveCamera(yfov=np.pi / 3.0)
     s = np.sqrt(2)/2
     scene.add(camera, pose=camera_mat)
-    light = pyrender.SpotLight(color=np.ones(3), intensity=3.0,
+    light = pyrender.SpotLight(color=np.ones(3), intensity=4.0,
                                innerConeAngle=np.pi/16.0)
     scene.add(light, pose=camera_mat)
+    light = pyrender.SpotLight(color=np.ones(3), intensity=6.0,
+                               innerConeAngle=0.2*np.pi)
+    light_pose = np.array([
+        [0,1,0,0],
+        [0,0,1,1],
+        [1,0,0,0],
+        [0,0,0,1]
+    ], dtype=np.float32)
+    scene.add(light, pose=light_pose)
     r = pyrender.OffscreenRenderer(512, 512)
     color, depth = r.render(scene)
     if return_depth:
